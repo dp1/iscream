4bf287e7bce4c5077e6d96a4d67378e55b6a39cc
Dario Petrillo <dario.pk1@gmail.com>
2022-06-02 21:21:49 +0200

    Handle shadow updates coming from the webif

--- device/main.c
+++ device/main.c
@@ -22,7 +22,88 @@ gpio_t ir_pin = GPIO_PIN(PORT_A, 4);
 gpio_t ckout = GPIO_PIN(PORT_E, 9);
 gpio_t datin2 = GPIO_PIN(PORT_E, 7);
 
-void on_pub(const emcute_topic_t *topic, void *data, size_t len) {
+typedef enum {
+    IDLE = 0,
+    ACTIVE = 1,
+    TRIGGERED = 2
+} state_t;
+
+state_t device_state = IDLE;
+
+// Report the current device state to the device shadow
+void report_state(void) {
+    bool active = device_state != IDLE;
+    bool triggered = device_state == TRIGGERED;
+
+    char state_buf[128];
+    sprintf(state_buf, "{\"state\": {\"reported\": {\"active\": %d, \"triggered\": %d}}}", active, triggered);
+
+    mqtt_pub("$aws/things/iscream/shadow/update", state_buf, 1);
+}
+
+typedef struct {
+    char *data;
+    char *end;
+} jsondict_t;
+
+// Get a value out of a json dictionary
+// Returns a pointer into the original string where the value starts
+// Returns {NULL,NULL} if no such child exists
+jsondict_t json_get_child(jsondict_t *json, char *key) {
+    jsondict_t res = {NULL, NULL};
+
+    if(json == NULL || json->data == NULL || json->data[0] != '{') {
+        return res;
+    }
+
+    size_t keylen = strlen(key);
+
+    for(char *p = json->data + 1; p < json->end; ++p) {
+        if(*p == ' ') continue;
+
+        // start of a key
+        if(*p == '\"') {
+
+            p++;
+            char *foundkey = p;
+            while(p < json->end && *p != '\"') p++;
+            if(p == json->end) return res;
+            p++;
+
+            if(*p != ':') return res;
+            p++;
+            char *foundvalue = p;
+
+            // Skip the value
+            int depth = 0;
+            bool in_string = false;
+
+            while(p < json->end) {
+                if(*p == '\"') in_string = !in_string;
+                else if(*p == '{' && !in_string) ++depth;
+                else if(*p == '}' && !in_string) --depth;
+
+                // End of value found
+                if(!in_string && depth == 0 && *p == ',') break;
+                if(!in_string && depth == -1 && *p == '}') break;
+
+                ++p;
+            }
+
+            if(!strncmp(key, foundkey, keylen)) {
+                // Target found
+                res.data = foundvalue;
+                res.end = p;
+                return res;
+            }
+        }
+    }
+
+    // Not found
+    return res;
+}
+
+void on_shadow_update(const emcute_topic_t *topic, void *data, size_t len) {
     char *in = (char *)data;
     printf("### got publication for topic '%s' [%i] ###\n",
            topic->name, (int)topic->id);
@@ -32,6 +113,17 @@ void on_pub(const emcute_topic_t *topic, void *data, size_t len) {
     }
     puts("");
 
+    jsondict_t json = {.data = data, .end = data + len};
+    jsondict_t state = json_get_child(&json, "state");
+    jsondict_t delta = json_get_child(&state, "desired");
+    jsondict_t active = json_get_child(&delta, "active");
+    if(!active.data) return;
+
+    // state_t old_state = device_state;
+    if(active.data[0] == '0' || active.data[0] == 'f') device_state = IDLE;
+    else device_state = ACTIVE;
+
+    printf("Received shadow delta, new state %d\n", device_state);
 }
 
 void audio_cb(double dB) {
@@ -39,25 +131,6 @@ void audio_cb(double dB) {
     printf("%3d\n", db);
     // for(int i = 30; i < db; i++) putchar('#');
     // putchar('\n');
-}
-
-typedef enum {
-    IDLE = 0,
-    ACTIVE = 1,
-    TRIGGERED = 2
-} state_t;
-
-state_t state = IDLE;
-
-// Report the current device state to the device shadow
-void report_state(void) {
-    bool active = state != IDLE;
-    bool triggered = state == TRIGGERED;
-
-    char state_buf[128];
-    sprintf(state_buf, "{\"state\": {\"reported\": {\"active\": %d, \"triggered\": %d}}}", active, triggered);
-
-    mqtt_pub("$aws/things/iscream/shadow/update", state_buf, 1);
 }
 
 #define IR_BUTTON_ON 0xA2
@@ -78,30 +151,23 @@ void* ir_remote_thread(void *arg) {
             continue;
         }
 
-        state_t old_state = state;
-
         switch(cmd.cmd) {
             case IR_BUTTON_ON:
                 puts("Turning on");
-                state = ACTIVE;
+                device_state = ACTIVE;
                 break;
             case IR_BUTTON_OFF:
                 puts("Turning off");
-                state = IDLE;
+                device_state = IDLE;
                 break;
             case IR_BUTTON_STOP:
-                if(state == TRIGGERED) {
+                if(device_state == TRIGGERED) {
                     puts("Stopping alarm");
-                    state = ACTIVE;
+                    device_state = ACTIVE;
                 } else {
                     puts("Stop command received, but alarm was't triggered - ignoring");
                 }
                 break;
-        }
-
-        // Update device shadow on state change
-        if(old_state != state) {
-            report_state();
         }
     }
     return NULL;
@@ -109,16 +175,23 @@ void* ir_remote_thread(void *arg) {
 
 const char* mqtt_subs[] = {
     "$aws/things/iscream/shadow/update/accepted",
-    "$aws/things/iscream/shadow/get/accepted",
     NULL
 };
 
 int main(void) {
-
     ir_remote_init(&remote, ir_pin);
     audio_init(ckout, datin2, audio_cb);
-    audio_start();
-    setup_mqtt(mqtt_subs, on_pub);
+    setup_mqtt(mqtt_subs, on_shadow_update);
 
+    audio_start();
     thread_create(ir_thread_stack, sizeof(ir_thread_stack), THREAD_PRIORITY_MAIN - 1, 0, ir_remote_thread, NULL, "ir_remote");
+
+    state_t old_state = device_state;
+    while(1) {
+        if(old_state != device_state) {
+            report_state();
+            old_state = device_state;
+        }
+        xtimer_usleep(100000);
+    }
 }
